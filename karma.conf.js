de1026a9410ce99f3851a3967c00fe4c310bb697
David Santiago <david@davidsantiago.net>
2016-01-28 08:58:16 +0100

    Más modificaciones

--- karma.conf.js
+++ karma.conf.js
@@ -5,16 +5,24 @@ module.exports = function (config) {
             'js/jquery/jquery-2.1.1.min.js',
             'js/plugins/jquery-ui/jquery-ui.js',
             
-            'js/angular/angular.js',
+            'js/angular/angular.min.js',
             'js/angular/angular-mocks.js',
             'js/angular/angular-sanitize.js',
-            'js/ui-router/angular-ui-router.js',
-            'js/plugins/oclazyload/dist/ocLazyLoad.js',
             
-            'js/angular-translate/angular-translate.js',
-            'js/bootstrap/bootstrap.js',
-            'js/bootstrap/ui-bootstrap-tpls-0.12.0.js',
+            'js/plugins/oclazyload/dist/ocLazyLoad.min.js',
+            'js/angular-translate/angular-translate.min.js',
+            'js/bootstrap/bootstrap.min.js',
+            'js/bootstrap/ui-bootstrap-tpls-0.12.0.min.js',
             'js/plugins/angular-idle/angular-idle.js',
+
+            'js/plugins/footable/footable.all.min.js',
+            'js/plugins/footable/angular-footable.js',
+            'js/plugins/moment/moment.min.js',
+            'js/plugins/moment/locale/es.js',
+            'js/plugins/datapicker/angular-datepicker.js',
+            'js/plugins/clockpicker/clockpicker.js',
+            'js/plugins/chosen/chosen.jquery.js',
+            'js/plugins/chosen/chosen.js',
 
             'js/app/**/*.js',
             'tests/**/*.js'
