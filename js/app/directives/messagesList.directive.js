ec26d03a204f76157b1a03c5d32b36b33764568b
David Santiago Fdez-Bermejo Morales <david@davidsantiago.net>
2016-01-29 13:47:00 +0100

    Cambio de nombre de un cliente

--- js/app/directives/messagesList.directive.js
+++ js/app/directives/messagesList.directive.js
@@ -5,14 +5,24 @@ function messagesList() {
     return {
 
         scope: {},
-        controller: function(Chats, $stateParams) {
+        controller: function(Chats, $stateParams, $timeout) {
             var id = $stateParams.id;
             var vm = this;
             vm.chat = {};
 
+            var eo = null;
+
             Chats.get(id, function(chat) {
+                console.log(chat);
                 vm.chat = chat;
+
+                eo = chat;
             });
+
+            $timeout(function() {
+                vm.chat = eo;
+
+            }, 2000);
 
         },
         controllerAs: 'vm',
