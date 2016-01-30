e9e7b2f3dd5346c69f5b6b6ca814486acbde0660
David Santiago <david@davidsantiago.net>
2016-01-30 20:58:14 +0100

    Reemplazo de caracter / en fechas mal formateadas

--- js/app/services/helper.service.js
+++ js/app/services/helper.service.js
@@ -4,6 +4,7 @@ angular.module('inspinia')
 function helperService() {
     return {
         transformDate: function(date) {
+            date = date.split("/").join("-");
             var ary = date.split('-');
             return ary[1] + '-' + ary[0] + '-' + ary[2];
         }
